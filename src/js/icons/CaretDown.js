fd5a8d25a073fe4b79f2a00dc77e6b10b765e09a
Alan Souza <alansouzati@gmail.com>
2017-09-12 10:21:00 -0700

    Renamed CarrotUp and CarrotDown

--- src/js/icons/CaretDown.js
+++ src/js/icons/CaretDown.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Icon from '../Icon';
 
-const CarrotDown = props => (
-  <Icon a11yTitle='CarrotDown' {...props}>
+const CaretDown = props => (
+  <Icon a11yTitle='CaretDown' {...props}>
     <polygon fill='none' stroke='#000' strokeWidth='2' points='22 8 12 20 2 8' />
   </Icon>
 );
 
-export default CarrotDown;
+export default CaretDown;
--- src/js/icons/CaretUp.js
+++ src/js/icons/CaretUp.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Icon from '../Icon';
 
-const CarrotUp = props => (
-  <Icon a11yTitle='CarrotUp' {...props}>
+const CaretUp = props => (
+  <Icon a11yTitle='CaretUp' {...props}>
     <polygon fill='none' stroke='#000' strokeWidth='2' points='22 4 12 16 2 4' transform='matrix(1 0 0 -1 0 20)' />
   </Icon>
 );
 
-export default CarrotUp;
+export default CaretUp;
