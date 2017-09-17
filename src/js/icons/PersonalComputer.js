7268da084f16fab7cc27855e07f3135fc286a649
Alan Souza <alansouzati@gmail.com>
2017-09-16 19:27:02 -0700

    Updated Instagram and PersonalComputer icons

--- src/js/icons/PersonalComputer.js
+++ src/js/icons/PersonalComputer.js
@@ -4,7 +4,7 @@ import Icon from '../Icon';
 
 const PersonalComputer = props => (
   <Icon viewBox='0 0 24 24' a11yTitle='PersonalComputer' {...props}>
-    <path fill='none' stroke='#000' strokeWidth='2' d='M3,18 L21,18 L21,4 L3,4 L3,18 Z M1,21 L23,21 L23,18 L1,18 L1,21 Z M6,15 L18,15 L18,7 L6,7 L6,15 Z' />
+    <path fill='none' stroke='#000' strokeWidth='2' d='M3,18 L21,18 L21,5 L21,5 C21,4.44771525 20.5522847,4 20,4 L4,4 L4,4 C3.44771525,4 3,4.44771525 3,5 L3,18 Z M2,20 L22,20 C23,20 23,19 23,19 L1,19 C1,19 1,20 2,20 Z' />
   </Icon>
 );
 
