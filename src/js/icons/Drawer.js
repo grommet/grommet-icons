c57aa33fdfadda183851e61e054ab880eeafb1f0
Tracy Barmore <tracy.barmore@hpe.com>
2017-11-06 17:49:52 -0800

    Adding Drawer, Network, VirtualStorage, Storage

---
+++ src/js/icons/Drawer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Icon from '../Icon';
+
+const Drawer = props => (
+  <Icon viewBox='0 0 24 24' a11yTitle='Drawer' {...props}>
+    <path fill='none' stroke='#000000' strokeWidth='2' d='M1,14 L6,2 L18,2 L23,14 L21,22 L3,22 L1,14 Z M1,14 L23,14' />
+  </Icon>
+);
+
+export default Drawer;
---
+++ src/js/icons/Network.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Icon from '../Icon';
+
+const Network = props => (
+  <Icon viewBox='0 0 24 24' a11yTitle='Network' {...props}>
+    <path fill='none' stroke='#000000' strokeWidth='2' d='M7.5,7 L7.5,15.5142857 C7.5,19.7571429 13,18.3428571 13,22.1142857 L13,24 M11,10 L7.5,7 L4,10 M16.5,2 L16.5,10.440516 C16.5,14.5083612 13,13.1524128 13,16.7682752 L13,24 M13,5 L16.5,2 L20,5' />
+  </Icon>
+);
+
+export default Network;
