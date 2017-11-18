3d868367e3724a7fa0d883a5f9892d38da20833a
Tracy Barmore <tracy.barmore@hpe.com>
2017-11-17 16:21:08 -0800

    Added Flag-fill, Projects, and changed Chat

---
+++ src/js/icons/FlagFill.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Icon from '../Icon';
+
+const FlagFill = props => (
+  <Icon viewBox='0 0 24 24' a11yTitle='FlagFill' {...props}>
+    <path fill='#000000' fillRule='evenodd' stroke='#000000' strokeWidth='2' d='M2,24 L2,2 C10,-1.5238864 13,6.64375708 22,2 L22,14 C14,18.895222 9,9.89651097 2,14' />
+  </Icon>
+);
+
+export default FlagFill;
