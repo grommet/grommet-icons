f793952b45e447e0a76ab70dd615577030197287
Alan Souza <alansouzati@gmail.com>
2017-11-21 17:00:17 -0800

    v0.14.0: added blank icon

---
+++ src/js/Blank.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Icon from './Icon';
+
+const Blank = props => (
+  <Icon viewBox='0 0 24 24' aria-hidden={true} {...props} />
+);
+
+export default Blank;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import StyledIcon from './StyledIcon';
 
 export * from './icons';
+export { default as Blank } from './Blank';
 
 export default StyledIcon;
