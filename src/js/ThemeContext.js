071ccb4bdb27e2c387470aabbf23bcb647bb8ac2
Eric Soderberg <eric.soderberg@hpe.com>
2018-05-30 13:06:20 -0700

    Added ThemeContext. Update to React 16.3. (#51)
    * Added ThemeContext. Update to React 16.3.
    
    * Removed deep-assign dependency
    
    * Added colors to theme

---
+++ src/js/ThemeContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import defaultTheme from './defaultTheme';
+
+const ThemeContext = React.createContext(defaultTheme);
+
+export default ThemeContext;
---
+++ src/js/defaultTheme.js
@@ -0,0 +1,8 @@
+export default {
+  color: '#666666',
+  colors: {},
+  size: {
+    large: '48px',
+    xlarge: '96px',
+  },
+};
