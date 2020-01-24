6dfa0515745bbd1c28430a7a2ce660aef23c70f0
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-24 12:58:46 -0800

    Update icon size/dimension calculation

--- src/js/StyledIcon.js
+++ src/js/StyledIcon.js
@@ -6,8 +6,16 @@ import { colorStyle } from 'grommet-styles';
 import { defaultProps } from './default-props';
 
 const colorCss = css`
-  ${props => colorStyle('fill', props.color || props.theme.global.colors.icon, props.theme)}
-  ${props => colorStyle('stroke', props.color || props.theme.global.colors.icon, props.theme)}
+  ${props => colorStyle(
+      'fill',
+      props.color || props.theme.global.colors.icon,
+      props.theme
+    )}
+  ${props => colorStyle(
+      'stroke',
+      props.color || props.theme.global.colors.icon,
+      props.theme
+    )}
 
   g {
     fill: inherit;
@@ -37,17 +45,39 @@ const colorCss = css`
 
 const IconInner = ({
  a11yTitle, color, size, theme, ...rest
-}) => <svg aria-label={a11yTitle} {...rest} />;
+}) => (
+  <svg aria-label={a11yTitle} {...rest} />
+);
 IconInner.displayName = 'Icon';
+
+const parseMetricToNum = string => parseFloat(string.match(/\d+(\.\d+)?/), 10);
 
 const StyledIcon = styled(IconInner)`
   display: inline-block;
   flex: 0 0 auto;
 
-  ${({ size = 'medium', theme }) => `
-    width: ${theme.icon.size[size] || size};
-    height: ${theme.icon.size[size] || size};
-  `}
+  ${({ size = 'medium', theme, viewBox }) => {
+    const [, , w, h] = (viewBox || '0 0 24 24').split(' ');
+    const scale = w / h;
+    const dimension = parseMetricToNum(theme.icon.size[size] || size);
+
+    if (w < h) {
+      return `
+      width: ${dimension}px;
+      height: ${dimension / scale}px;
+    `;
+    }
+    if (h < w) {
+      return `
+      width: ${dimension * scale}px;
+      height: ${dimension}px;
+    `;
+    }
+    return `
+      width: ${dimension}px;
+      height: ${dimension}px;
+    `;
+  }}
   ${({ color }) => color !== 'plain' && colorCss}
   ${({ theme }) => theme && theme.icon.extend}
 `;
