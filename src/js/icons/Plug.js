786b45c3836c8e66d9e6d2ce43de689a7333ea00
Taylor Seamans <taylor.seamans@yahoo.com>
2021-09-14 11:20:56 -0700

    Lint fix on files

--- src/js/icons/Plug.js
+++ src/js/icons/Plug.js
@@ -4,7 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Plug = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Plug" {...props}>
-    <path stroke='#000' strokeWidth='2' d='M15 6V1m-3 23v-9M9 6V1M6 6h12v7a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V6z' />
+    <path stroke="#000" strokeWidth="2" d="M15 6V1m-3 23v-9M9 6V1M6 6h12v7a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V6z" />
   </StyledIcon>
 ));
 
--- src/js/icons/Sans.js
+++ src/js/icons/Sans.js
@@ -4,7 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Sans = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Sans" {...props}>
-    <path stroke='#000' strokeWidth='2' d='M1 12h22M2 22h20a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1zM5 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2z' />
+    <path stroke="#000" strokeWidth="2" d="M1 12h22M2 22h20a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1zM5 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
   </StyledIcon>
 ));
 
