b5b134d2536cb8f84eaa65d099eb56e3adaa402a
Taylor Seamans <taylor.seamans@yahoo.com>
2021-10-01 11:01:00 -0700

    Update icons to use stroke instead of fill

--- src/js/icons/Ascending.js
+++ src/js/icons/Ascending.js
@@ -4,7 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Ascending = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Ascending" {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="m12.08 7.286.354-.353.354.353L17 11.498l-.707.708-3.358-3.359V17h-1V8.847l-3.359 3.359-.707-.708 4.212-4.212z" fill="#000" />
+    <path stroke="#000" strokeWidth="2" d="m12.08 7.286.354-.353.354.353L17 11.498l-.707.708-3.358-3.359V17h-1V8.847l-3.359 3.359-.707-.708 4.212-4.212z" clipRule="evenodd" />
   </StyledIcon>
 ));
 
