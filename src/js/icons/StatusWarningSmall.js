49e71b7d0bcce6f883afb589c1d587f95c64d5a1
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-10-06 14:07:25 -0600

    change a11ytitle on status icons

--- src/js/icons/StatusWarningSmall.js
+++ src/js/icons/StatusWarningSmall.js
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusWarningSmall = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="StatusWarningSmall" {...props}>
+  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="StatusWarning" {...props}>
     <path fillRule="evenodd" stroke="#000" strokeLinejoin="round" d="m6 1 5 9H1z" />
   </StyledIcon>
 ));
