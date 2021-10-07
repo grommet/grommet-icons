81281bb85551f499cb51f2d03f7cc7b639ce8bbd
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-10-07 13:51:41 -0600

    change status good to say ok

--- src/js/icons/StatusGoodSmall.js
+++ src/js/icons/StatusGoodSmall.js
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusGoodSmall = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="good status" {...props}>
+  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="ok status" {...props}>
     <circle cx="6" cy="6" r="5" fillRule="evenodd" stroke="#000" />
   </StyledIcon>
 ));
