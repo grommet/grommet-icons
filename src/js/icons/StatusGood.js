969dcbcfab731035b090761d8a1d08a3f1b130b0
britt6612 <42451602+britt6612@users.noreply.github.com>
2021-10-08 14:43:12 -0600

    Update src/js/icons/StatusGood.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- src/js/icons/StatusGood.js
+++ src/js/icons/StatusGood.js
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusGood = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Ok Status" {...props}>
+  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Okay Status" {...props}>
     <path
       fill="none"
       stroke="#000"
