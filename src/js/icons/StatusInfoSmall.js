764eb37289d9c31d7570f1ea0fc9f299a75bc0fb
britt6612 <42451602+britt6612@users.noreply.github.com>
2021-10-10 13:52:55 -0600

    Update src/js/icons/StatusInfoSmall.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- src/js/icons/StatusInfoSmall.js
+++ src/js/icons/StatusInfoSmall.js
@@ -3,7 +3,7 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusInfoSmall = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="Info Status" {...props}>
+  <StyledIcon ref={ref} viewBox="0 0 12 12" a11yTitle="Information Status" {...props}>
     <rect
       width="10"
       height="10"
