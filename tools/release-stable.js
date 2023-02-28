286300a1155ffe3ebe3f05b32fa8a9625e9cd537
Eric Soderberg <eric.soderberg@hpe.com>
2023-02-28 09:55:08 -0800

    Updated release-stable

--- tools/release-stable.js
+++ tools/release-stable.js
@@ -1,15 +1,16 @@
-import path from 'path';
-import git from 'simple-git';
 import del from 'del';
 import fs from 'fs-extra';
+import git from 'simple-git/promise';
+import path from 'path';
 
 const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/grommet-icons.git`;
-const localFolder = path.resolve('./.tmp/grommet-icons');
-const localDist = path.resolve('./dist');
+const localFolder = path.resolve('.tmp/grommet-icons');
+const localDist = path.resolve('dist');
 
 if (process.env.CI) {
   del(localFolder).then(() => {
-    git().silent(true)
+    git()
+      .silent(false)
       .clone(repoURL, localFolder)
       .then(() => git(localFolder).checkout('stable'))
       .then(() => del([`${localFolder}/**/*`]))
@@ -17,8 +18,10 @@ if (process.env.CI) {
       .then(() => git(localFolder).add(['--all', '.']))
       .then(() => git(localFolder).commit('stable updated'))
       .then(() => git(localFolder).push('origin', 'stable'))
-      .catch(err => console.error('failed: ', err));
+      .catch((err) => console.error('failed: ', err));
   });
 } else {
-  console.warn('Skipping release. Release:stable task should be executed by CI only.');
+  console.warn(
+    'Skipping release. Release:stable task should be executed by CI only.',
+  );
 }
