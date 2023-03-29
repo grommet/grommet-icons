b3348869fdeccc10fb3c3894c7fcd9af4afc24d8
Eric Soderberg <eric.soderberg@hpe.com>
2023-03-29 15:55:02 -0700

    Fixed icons that were incorrectly getting a fill when  was applied. Fixes issue #275.

--- src/js/icons/Flows.js
+++ src/js/icons/Flows.js
@@ -4,7 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Flows = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Flows" {...props}>
-    <path stroke="#000" strokeWidth="2" d="M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5zm0 11a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z" />
+    <path stroke="#000" strokeWidth="2" fill="none" d="M3 5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5zm0 11a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z" />
   </StyledIcon>
 ));
 
--- src/js/icons/Memory.js
+++ src/js/icons/Memory.js
@@ -4,7 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Memory = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Memory" {...props}>
-    <path stroke="#000" strokeWidth="2" d="M10 18h4m-4-4h4m-4-4h4m-4-4h4m6 12h3m-3-4h3m-3-4h3m-3-4h3M1 18h3m-3-4h3m-3-4h3M1 6h3m11 14h4a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1zM5 20h4a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1z" />
+    <path stroke="#000" strokeWidth="2" fill="none" d="M10 18h4m-4-4h4m-4-4h4m-4-4h4m6 12h3m-3-4h3m-3-4h3m-3-4h3M1 18h3m-3-4h3m-3-4h3M1 6h3m11 14h4a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1zM5 20h4a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1z" />
   </StyledIcon>
 ));
 
