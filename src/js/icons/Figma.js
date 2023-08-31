1e749752c934104ddca0d8d708c301d3e51cee2c
Taylor Seamans <taylor.seamans@yahoo.com>
2023-08-31 10:00:42 -0700

    Dont modify social icons

--- src/js/icons/Figma.js
+++ src/js/icons/Figma.js
@@ -4,23 +4,13 @@ import { StyledIcon } from '../StyledIcon';
 
 const Figma = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Figma" {...props}>
-    <path
-      fill="#0ACF83"
-      d="M8 24c2.208 0 4-1.792 4-4v-4H8c-2.208 0-4 1.792-4 4s1.792 4 4 4Z"
-    />
-    <path
-      fill="#A259FF"
-      d="M4 12c0-2.208 1.792-4 4-4h4v8H8c-2.208 0-4-1.792-4-4Z"
-    />
-    <path
-      fill="#F24E1E"
-      d="M4 4c0-2.208 1.792-4 4-4h4v8H8C5.792 8 4 6.208 4 4Z"
-    />
-    <path fill="#FF7262" d="M12 0h4c2.208 0 4 1.792 4 4s-1.792 4-4 4h-4V0Z" />
-    <path
-      fill="#1ABCFE"
-      d="M20 12c0 2.208-1.792 4-4 4s-4-1.792-4-4 1.792-4 4-4 4 1.792 4 4Z"
-    />
+    <g fill="none" fillRule="evenodd" transform="translate(4)">
+      <circle cx="12" cy="12" r="4" fill="#19BCFE" />
+      <path fill="#09CF83" d="M4 24a4 4 0 0 0 4-4v-4H4a4 4 0 1 0 0 8z" />
+      <path fill="#A259FF" d="M4 16h4V8H4a4 4 0 1 0 0 8z" />
+      <path fill="#F24E1E" d="M4 8h4V0H4a4 4 0 1 0 0 8z" />
+      <path fill="#FF7262" d="M12 8H8V0h4a4 4 0 1 1 0 8z" />
+    </g>
   </StyledIcon>
 ));
 
--- src/js/icons/Mail.js
+++ src/js/icons/Mail.js
@@ -7,8 +7,7 @@ const Mail = forwardRef((props, ref) => (
     <path
       fill="#01A982"
       fillRule="evenodd"
-      d="M2 3a2 2 0 0 0-2 2v.465a1 1 0 0 0 .445.832l10.446 6.963a2 2 0 0 0 2.218 0l10.446-6.963A1 1 0 0 0 24 5.465V5a2 2 0 0 0-2-2H2Zm1 19h18a3 3 0 0 0 3-3V8l-10.336 6.89a3 3 0 0 1-3.328 0L0 8v11a3 3 0 0 0 3 3Z"
-      clipRule="evenodd"
+      d="M23 20V6l-11 9L1 6v14h22zm-11-8 10-8H2l10 8z"
     />
   </StyledIcon>
 ));
