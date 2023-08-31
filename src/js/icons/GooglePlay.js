673c58bb4b512dd38783c4530f05afaab8107cfd
Taylor Seamans <taylor.seamans@yahoo.com>
2023-08-31 10:13:14 -0700

    Remove modifications to icons

--- src/js/icons/GooglePlay.js
+++ src/js/icons/GooglePlay.js
@@ -4,10 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const GooglePlay = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="GooglePlay" {...props}>
-    <g fill="none" stroke="#444" strokeWidth="2">
-      <path strokeLinejoin="round" d="M3 2v20l18-10z" />
-      <path d="m3 2 11 14M3 22 14 8" />
-    </g>
+    <g fill="none" stroke="#444" strokeWidth="2"><path strokeLinejoin="round" d="M3 2v20l18-10z" /><path d="m3 2 11 14M3 22 14 8" /></g>
   </StyledIcon>
 ));
 
--- src/js/icons/Mail.js
+++ src/js/icons/Mail.js
@@ -4,11 +4,7 @@ import { StyledIcon } from '../StyledIcon';
 
 const Mail = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Mail" {...props}>
-    <path
-      fill="#01A982"
-      fillRule="evenodd"
-      d="M23 20V6l-11 9L1 6v14h22zm-11-8 10-8H2l10 8z"
-    />
+    <path fill="#01A982" fillRule="evenodd" d="M23 20V6l-11 9L1 6v14h22zm-11-8 10-8H2l10 8z" />
   </StyledIcon>
 ));
 
