14648fa4610f91bbd7ba57b87597b5d3884a430a
Taylor Seamans <taylor.seamans@yahoo.com>
2025-07-22 15:09:16 -0700

    Update release stable to support NEXT-stable (#358)
    * Update release stable t accommodate NEXT-stable
    
    * Update circleci

--- tools/release-stable.js
+++ tools/release-stable.js
@@ -6,19 +6,25 @@ import debug from 'debug';
 
 const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/grommet-icons.git`;
 const localFolder = path.resolve('.tmp/grommet-icons');
-const localDist = path.resolve('dist');
+const BUILD_DIR = 'dist';
+const localDist = path.resolve(BUILD_DIR);
+
+const [BRANCH] = process.argv
+  .find((v) => v.includes('--branch='))
+  .split('=')
+  .slice(-1);
 
 if (process.env.CI) {
   debug.enable('simple-git,simple-git:*');
   del(localFolder).then(() => {
     git()
       .clone(repoURL, localFolder)
-      .then(() => git(localFolder).checkout('stable'))
+      .then(() => git(localFolder).checkout(BRANCH))
       .then(() => del([`${localFolder}/**/*`]))
       .then(() => fs.copy(localDist, localFolder))
       .then(() => git(localFolder).add(['--all', '.']))
-      .then(() => git(localFolder).commit('stable updated'))
-      .then(() => git(localFolder).push('origin', 'stable'))
+      .then(() => git(localFolder).commit(`${BRANCH} updated`))
+      .then(() => git(localFolder).push('origin', BRANCH))
       .catch((err) => console.error('failed: ', err));
   });
 } else {
