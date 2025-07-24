f1ec63c7f00d08a3fb26063396caa797c92f31dc
taysea <taylor.seamans@yahoo.com>
2025-07-24 11:13:54 -0700

    Change fill to #000

--- src/js/icons/CaretLeftFill.js
+++ src/js/icons/CaretLeftFill.js
@@ -2,8 +2,16 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const CaretLeftFill = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="CaretLeftFill" {...props}>
-    <path d="M18.0001 19.5858C17.9999 21.3674 15.846 22.2595 14.5861 20.9999L7.00013 13.414C6.2191 12.6329 6.21913 11.3669 7.00013 10.5858L14.5861 2.9999C15.846 1.74013 18.0001 2.63224 18.0001 4.41396V19.5858Z" fill="#555555"/>
+  <StyledIcon
+    ref={ref}
+    viewBox="0 0 24 24"
+    a11yTitle="CaretLeftFill"
+    {...props}
+  >
+    <path
+      d="M18.0001 19.5858C17.9999 21.3674 15.846 22.2595 14.5861 20.9999L7.00013 13.414C6.2191 12.6329 6.21913 11.3669 7.00013 10.5858L14.5861 2.9999C15.846 1.74013 18.0001 2.63224 18.0001 4.41396V19.5858Z"
+      fill="#000"
+    />
   </StyledIcon>
 ));
 
--- src/js/icons/Template.js
+++ src/js/icons/Template.js
@@ -3,7 +3,12 @@ import { StyledIcon } from '../StyledIcon';
 
 const Template = forwardRef((props, ref) => (
   <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Template" {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="M18 1C20.7614 1 23 3.23858 23 6V18C23 20.7614 20.7614 23 18 23H6C3.23858 23 1 20.7614 1 18V6C1 3.23858 3.23858 1 6 1H18ZM3 18C3 19.6569 4.34315 21 6 21H7V9H3V18ZM9 21H18C19.6569 21 21 19.6569 21 18V9H9V21ZM6 3C4.34315 3 3 4.34315 3 6V7H21V6C21 4.34315 19.6569 3 18 3H6Z" fill="#555555"/>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M18 1C20.7614 1 23 3.23858 23 6V18C23 20.7614 20.7614 23 18 23H6C3.23858 23 1 20.7614 1 18V6C1 3.23858 3.23858 1 6 1H18ZM3 18C3 19.6569 4.34315 21 6 21H7V9H3V18ZM9 21H18C19.6569 21 21 19.6569 21 18V9H9V21ZM6 3C4.34315 3 3 4.34315 3 6V7H21V6C21 4.34315 19.6569 3 18 3H6Z"
+      fill="#000"
+    />
   </StyledIcon>
 ));
 
