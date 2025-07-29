657440598c72d2efcfbce830f0ae37c265128451
taysea <taylor.seamans@yahoo.com>
2025-07-29 15:02:24 -0700

    Remove custom colors on status fill icons

--- src/js/icons/StatusUnknownFill.js
+++ src/js/icons/StatusUnknownFill.js
@@ -2,8 +2,18 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusUnknownFill = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="StatusUnknownFill" {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="M18 1C20.7614 1 23 3.23858 23 6V18C23 20.7614 20.7614 23 18 23H6C3.23858 23 1 20.7614 1 18V6C1 3.23858 3.23858 1 6 1H18Z" fill="#757575"/>
+  <StyledIcon
+    ref={ref}
+    viewBox="0 0 24 24"
+    a11yTitle="StatusUnknownFill"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M18 1C20.7614 1 23 3.23858 23 6V18C23 20.7614 20.7614 23 18 23H6C3.23858 23 1 20.7614 1 18V6C1 3.23858 3.23858 1 6 1H18Z"
+      fill="#000"
+    />
   </StyledIcon>
 ));
 
