d5017fe87d83c3884dc814da40ac76fafc3ab7ce
SeamusLeonardHPE <seamus.leonard@hpe.com>
2025-08-25 16:56:22 +0100

    Hpe Icon set updates
    Removed Logos
    Removed brandMarks
    Updated naming
    Removed -options

---
+++ src/js/icons/CaretLeft.js
@@ -0,0 +1,11 @@
+import React, { forwardRef } from 'react';
+import { StyledIcon } from '../StyledIcon';
+
+const CaretLeft = forwardRef((props, ref) => (
+  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="CaretLeft" {...props}>
+    <path fillRule="evenodd" clipRule="evenodd" d="M14.5862 3.00012C15.8461 1.7404 18.0002 2.63247 18.0003 4.41418V19.5861C18 21.3675 15.8461 22.2596 14.5862 21.0001L7.00025 13.4142C6.21922 12.6331 6.21925 11.3671 7.00025 10.5861L14.5862 3.00012ZM8.41431 12.0001L16.0003 19.5861V4.41418L8.41431 12.0001Z" fill="#555555"/>
+  </StyledIcon>
+));
+
+CaretLeft.displayName = 'CaretLeft';
+export { CaretLeft };
---
+++ src/js/icons/Left.js
@@ -0,0 +1,11 @@
+import React, { forwardRef } from 'react';
+import { StyledIcon } from '../StyledIcon';
+
+const Left = forwardRef((props, ref) => (
+  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="Left" {...props}>
+    <path d="M16.293 1.29302C16.6835 0.902491 17.3166 0.902491 17.7071 1.29302C18.0975 1.68355 18.0976 2.31658 17.7071 2.70708L8.41411 12L17.7071 21.293C18.0975 21.6835 18.0976 22.3166 17.7071 22.7071C17.3166 23.0976 16.6835 23.0975 16.293 22.7071L6.29302 12.7071C5.90249 12.3166 5.90249 11.6835 6.29302 11.293L16.293 1.29302Z" fill="#555555"/>
+  </StyledIcon>
+));
+
+Left.displayName = 'Left';
+export { Left };
