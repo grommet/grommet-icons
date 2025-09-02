11cb46d63cd529e1972ab12811194a1a5331b8c1
SeamusLeonardHPE <seamus.leonard@hpe.com>
2025-09-02 11:51:04 +0100

    Updated svgs forms

--- src/js/icons/StatusWarningFill.js
+++ src/js/icons/StatusWarningFill.js
@@ -2,8 +2,16 @@ import React, { forwardRef } from 'react';
 import { StyledIcon } from '../StyledIcon';
 
 const StatusWarningFill = forwardRef((props, ref) => (
-  <StyledIcon ref={ref} viewBox="0 0 24 24" a11yTitle="StatusWarningFill" {...props}>
-    <path fillRule="evenodd" clipRule="evenodd" d="M11.1258 3.67358C11.5068 2.98777 12.4932 2.98777 12.8742 3.67358L22.1746 20.4145C22.5449 21.081 22.063 21.9001 21.3005 21.9001H2.69951C1.93703 21.9001 1.45506 21.081 1.82536 20.4145L11.1258 3.67358Z" fill="#D36D00"/>
+  <StyledIcon
+    ref={ref}
+    viewBox="0 0 24 24"
+    a11yTitle="StatusWarningFill"
+    {...props}
+  >
+    <path
+      d="M9.3165 4.13051C10.4221 1.9194 13.5781 1.91937 14.6837 4.13051L21.4474 17.6588C22.4444 19.6535 20.9938 22.0006 18.7638 22.0006H5.23642C3.00636 22.0006 1.5557 19.6535 2.55283 17.6588L9.3165 4.13051Z"
+      fill="#D36D00"
+    />
   </StyledIcon>
 ));
 
